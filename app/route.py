30697c1df8ea7d2ce209048e846eba7c3508058f
waynnne <waynnnelee@gmail.com>
2019-11-18 23:46:24 +0800

    基本框架

--- app/route.py
+++ app/route.py
@@ -2,9 +2,18 @@
 # Author : waynnne
 # Date   : 2019-11-17 16:10
 
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, Flask!"
+
+@app.route('/home')
+def home():
+    return render_template("public/home.html")
+
+@app.route('/about')
+def about():
+    return render_template("public/about.html")
