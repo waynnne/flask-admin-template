e0245259c9319b38a7826f6cb10d8469bf657e8d
waynnne <waynnnelee@gmail.com>
2019-12-04 23:59:31 +0800

    注册error_handler

--- app/admin/public/views.py
+++ app/admin/public/views.py
@@ -5,7 +5,7 @@
 from flask import render_template
 from flask_login import login_required
 from app.admin.public import public_bp
-
+from app.extensions import db
 
 
 @public_bp.route('/')
@@ -23,3 +23,14 @@ def admin():
         'email': 'waynnne@mail.com'
     }
     return render_template("public/index.html", user=user)
+
+
+@public_bp.app_errorhandler(404)
+def not_fund_error(error):
+    return render_template('public/404.html'), 404
+
+
+@public_bp.app_errorhandler(500)
+def internal_server_error(error):
+    db.session.roll_back()
+    return render_template('public/500.html'), 500
